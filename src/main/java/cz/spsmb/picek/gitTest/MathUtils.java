8e4b7470804bc96f73224502f0809657cc228efe
Fildoo12 <filip.picek@gmail.com>
2023-12-20 11:19:08 +0100

    Task1 - implemented second power

---
+++ src/main/java/cz/spsmb/picek/gitTest/MathUtils.java
@@ -0,0 +1,7 @@
+package cz.spsmb.picek.gitTest;
+
+public class MathUtils {
+    public static final int secondPower(int number){
+        return number * number;
+    }
+}
---
+++ src/test/java/cz/spsmb/picek/gitTest/MathUtilsTest.java
@@ -0,0 +1,16 @@
+package cz.spsmb.picek.gitTest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class MathUtilsTest {
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,10})
+    void secondPowerTest(int number){
+        double expected = Math.pow(number,2);
+        double result = MathUtils.secondPower(number);
+        Assertions.assertEquals(expected, result);
+    }
+
+}
